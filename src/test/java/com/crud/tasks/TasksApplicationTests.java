b48d30a4f5ebb56877333c27c86aa8b97860d82b
rydzwr <rydzwr@gmail.com>
2022-02-27 22:36:16 +0100

    Connected To Database

---
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -0,0 +1,15 @@
+package com.crud.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
